bf0f7ae1dc7f1fb852d2100f19f5383ca6aa68c9
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-05-25 00:07:24 +0300

    in proccessssss

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const refs = {
+    select: document.querySelector('.breed-select'),
+    catInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error')
+}
+
+const API_key = 'live_fOxgLowEMvVzv83NVOkuO9MdqpbmCuvrfvPFk27zYDau65gVme3460SYk4qBg6tH';
+
+
+
+fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => response.json())
+    .then(cat => {
+        refs.error.setAttribute('hidden', '');
+        refs.loader.setAttribute('hidden', '');
+        const murkup = cat.map(c => `<option value="${c.id}">${c.name}</option>`).join('')
+        
+        refs.select.innerHTML = murkup;
+        refs.select.addEventListener('change', (e) => {
+            const findCat = cat.find(c => c.id === e.target.value);
+            //console.log(findCat.name);
+            fetch(`https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${findCat.id}&api_key=${API_key}`)
+                .then(data => data.json())
+                .then(cat => {
+                    const murkup = cat.map(c =>
+                        `<div class="cat">
+                            <h2>${c.breeds[0].name}</h2>
+                            <p>${c.breeds[0].description}</p>
+                            <p>Temperament: ${c.breeds[0].temperament}</p>
+                            <img src="${c.url}" width="340"/>
+                        </div>`).join('');                    
+                    refs.catInfo.innerHTML = murkup;
+                })
+                .catch(error => {
+                    refs.error.removeAttribute('hidden', '');
+                    console.log(error)
+                });
+        })
+    })
+    .catch(error => {
+        refs.error.removeAttribute('hidden', '');
+        console.log(error)
+    });
+refs.loader.removeAttribute('hidden', '')
+// refs.select.addEventListener('change', (e) => {
+//     console.log(e.target.value);
+
+// })
