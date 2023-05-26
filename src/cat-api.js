f2622f4beaaf91bfbd2ef5ae6baef04d930a71d7
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-05-26 23:13:58 +0300

    dont work

---
+++ src/cat-api.js
@@ -0,0 +1,13 @@
+const API_key = 'live_fOxgLowEMvVzv83NVOkuO9MdqpbmCuvrfvPFk27zYDau65gVme3460SYk4qBg6tH';
+
+function fetchBreeds() {
+    return fetch('https://api.thecatapi.com/v1/breeds')
+        .then(response => response.json());
+}
+
+function fetchCatByBreed(breedId){
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${breedId}&api_key=${API_key}`)
+        .then(data => data.json());
+}
+
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+
 const refs = {
     select: document.querySelector('.breed-select'),
     catInfo: document.querySelector('.cat-info'),
@@ -5,41 +7,38 @@ const refs = {
     error: document.querySelector('.error')
 }
 
-const API_key = 'live_fOxgLowEMvVzv83NVOkuO9MdqpbmCuvrfvPFk27zYDau65gVme3460SYk4qBg6tH';
-
-fetch('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.json())
-    .then(cat => {
-        refs.error.setAttribute('hidden', '');
-        refs.loader.setAttribute('hidden', '');
-        const murkup = cat.map(c => `<option value="${c.id}">${c.name}</option>`).join('');
         
-        refs.select.innerHTML = murkup;
-        refs.select.addEventListener('change', (e) => {
+refs.select.addEventListener('click', (e) => {
+    fetchBreeds()
+        .then(cat => {
+            const murkup = cat.map(c => `<option value="${c.id}">${c.name}</option>`).join('');
+        
+            refs.select.innerHTML = murkup;
+
             const findCat = cat.find(c => c.id === e.target.value);
-            //console.log(findCat.name);
-            fetch(`https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${findCat.id}&api_key=${API_key}`)
-                .then(data => data.json())
+               
+            fetchCatByBreed(findCat.id)
                 .then(cat => {
-                    const murkup = cat.map(c =>
-                        `   <img class="cat-img" src="${c.url}" width="340"/>
-                            <div class="cat-description">
-                                <h2>${c.breeds[0].name}</h2>
-                                <p>${c.breeds[0].description}</p>
-                                <p><h3>Temperament:</h3> ${c.breeds[0].temperament}</p>
-                            </div>                        
-                        `).join('');                    
-                    refs.catInfo.innerHTML = murkup;
+                    refs.catInfo.innerHTML = createMurkup(cat);
                 })
                 .catch(error => {
                     refs.error.removeAttribute('hidden', '');
                     console.log(error)
                 });
         })
-    })
-    .catch(error => {
-        refs.error.removeAttribute('hidden', '');
-        console.log(error)
-    });
-    
-refs.loader.removeAttribute('hidden', '')
+});
+
+function createMurkup(cat) { 
+    const murkup = cat.map(c =>
+        `   <img class="cat-img" src="${c.url}" width="340"/>
+                <div class="cat-description">
+                <h2>${c.breeds[0].name}</h2>
+                <p>${c.breeds[0].description}</p>
+                <p><h3>Temperament:</h3> ${c.breeds[0].temperament}</p>
+            </div>                        
+        `).join(''); 
+    return murkup;
+}    
+
+
+
